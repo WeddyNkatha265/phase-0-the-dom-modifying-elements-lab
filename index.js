09963df56e033e53e9d9aaa93b482a72327153a3
Weddy Nkatha <weddynkatha265@gmail.com>
2024-07-04 15:09:44 +0300

    Updates DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const removeMain = document.querySelector("main#main");
+removeMain.remove();
+// index.js
+const newHeader = document.querySelector("#victory");
+newHeader.textContent = "Your name is the champion!";
+// const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+// newHeader.id = 'victory';
+// newHeader.textContent = "Weddy is the champion";
